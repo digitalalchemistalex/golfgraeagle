5641b44d358fbbed745b44f554187390ea1dc5c3
GolfGraeagle <sean@zoomaway.com>
2026-04-06 02:53:16 +0000

    feat: add bachelor-party-golf-graeagle landing page with psychology template

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -91,6 +91,7 @@ ${url('/all-golf-courses','0.9','monthly')}
 ${url('/golf-trip-from-sacramento','0.9','monthly')}
 ${url('/summer-golf-graeagle','0.9','monthly')}
 ${url('/graeagle-golf-resort','0.9','monthly')}
+${url('/bachelor-party-golf-graeagle','0.9','monthly')}
 ${url('/trips','0.8','weekly')}
 ${url('/lodging','0.8','monthly')}
 ${url('/dining','0.8','monthly')}
